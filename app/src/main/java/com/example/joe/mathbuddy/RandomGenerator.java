e4a6c480c3ecb9b816181a738504e5d396b6541e
PaulNguyen2518 <35715419+PaulNguyen2518@users.noreply.github.com>
2018-11-15 20:28:15 -0600

    Add documentation, changed RandomGenerator method
    Added documenation for RandomGenerator and RandomProblemGenerator classes. Changed method setSeed() in RandomGenerator to accept an input of long type.

--- app/src/main/java/com/example/joe/mathbuddy/RandomGenerator.java
+++ app/src/main/java/com/example/joe/mathbuddy/RandomGenerator.java
@@ -3,7 +3,7 @@ package com.example.joe.mathbuddy;
 import java.util.Random;
 
 /**
- *
+ * Class that holds a static object of Random
  * @author Paul John Nguyen
  * @since 11-14-2018
  */
@@ -11,20 +11,30 @@ public class RandomGenerator
 {
     public static Random ran = new Random();
     public static long seed;
-    
+
+    /**
+     * Create new seed and set it for class field seed.
+     */
     public static void createSeed()
     {
         seed = ran.nextLong();
     }
-    
-    public static void setSeed()
+
+    /**
+     * Set a seed for Random ran via method setSeed(long).
+     * @param s long variable
+     */
+    public static void setSeed(long s)
     {
-        ran.setSeed(seed);
+        ran.setSeed(s);
     }
-    
+
+    /**
+     * Get seed for class field seed.
+     * @return long seed
+     */
     public static long getSeed()
     {
         return seed;
     }
-    
 }
--- app/src/main/java/com/example/joe/mathbuddy/RandomProblemGenerator.java
+++ app/src/main/java/com/example/joe/mathbuddy/RandomProblemGenerator.java
@@ -3,19 +3,19 @@ package com.example.joe.mathbuddy;
 import java.util.ArrayList;
 
 /**
- * Random problem generator class.
+ * Class that hold random problem generator methods.
  * @author Paul John Nguyen
  * @since 11-14-2018
  */
 public class RandomProblemGenerator 
 {
     /**
-     * 
+     * Generates a random number base on range given and whether or not it is a double or integer.
      * @param lowNumRange int for lower bound of number range.
      * @param highNumRange int for upper bound of number range.
      * @param decimalNums boolean to determine if number is non-integer.
      * @param numOfDecimals int for number of decimal places.
-     * @return 
+     * @return a number of Class Double
      */
     public static Double genNum(int lowNumRange, int highNumRange, boolean decimalNums, int numOfDecimals)
     {
@@ -30,15 +30,15 @@ public class RandomProblemGenerator
     }
     
     /**
-     * 
+     * Generates an ArrayList with object type Problem with ProblemType Basic.
      * @param numOfProb int for number of problems to be in the set.
-     * @param probOps ArrayList of type Character that stores operations to be in problems.
-     * @param lowProbLen 
-     * @param highProbLen
-     * @param lowNumRange
-     * @param highNumRange
-     * @param decimalNums
-     * @param numOfDecimals
+     * @param probOps ArrayList of type Character that stores operations to be in problems, i.e. any char from set: {'+', '-', '*', '/', '^'}.
+     * @param lowProbLen integer value lower bound for number of numbers within problem(s), e.g. 2 -> 1 + 2, 3 -> 1 + 2 + 3, must be greater than 2.
+     * @param highProbLen integer value upper bound for number of numbers within problem(s).
+     * @param lowNumRange integer value lower bound for range of number values.
+     * @param highNumRange integer value upper bound for range of number values.
+     * @param decimalNums boolean value to determine whether numbers are integers only or not, false for integers only.
+     * @param numOfDecimals integer value for number of decimal places to be in double.
      * @return basicProbSet ArrayList of type Problem
      */
     public static ArrayList<Problem> genBasicProbSet(int numOfProb, ArrayList<Character> probOps, int lowProbLen, int highProbLen, int lowNumRange, int highNumRange, boolean decimalNums, int numOfDecimals)
@@ -61,7 +61,7 @@ public class RandomProblemGenerator
            highProbLen = temp;
         }
         
-        //Checks problem length bounds, a problem cannot be lower than 2, that would just be a number. 
+        //Checks problem length bounds, a problem cannot be lower than 2, that would just be a number, should be checked for in activity creation screen.
         if(lowProbLen >= 2 || highProbLen >= 2)
         {
             ArrayList<Character> ops = new ArrayList();
@@ -74,7 +74,7 @@ public class RandomProblemGenerator
             for(i = 0; i < numOfProb; i++)
             {
                 probLen = NumberGenerator.intNumGen(lowProbLen, highProbLen);
-                //add operations to ops to be in problem
+                //add random operations to ops from given probOps to be in problem
                 for(j = 0; j < probLen - 1; j++)
                 {
                     ops.add(probOps.get(RandomGenerator.ran.nextInt(probOps.size())));
